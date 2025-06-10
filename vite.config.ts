b2d37f88e1754f89d6ee44b959c34b24e1e13f24
Krishna <Krishna1234shah@gmail.com>
2025-06-10 12:17:31 +0545

    Save local changes before pulling

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/KrishnaPortfolio/',  // <-- add your GitHub repo name here with trailing slash
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
