87850f54cd33863d809838fb2270e5c444333a9c
Krishna <Krishna1234shah@gmail.com>
2025-06-10 13:24:22 +0545

    Save old

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/KrishnaPortfolio/',  // <-- add your GitHub repo name here with trailing slash
   plugins: [react()],
-  build: {
-    outDir: 'dist'  // this is default, but make sure it's not changed
-  }
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
